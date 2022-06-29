e7b997652f0fbc03bfd7651148b4b0f63d7dd6f5
Yarixa Rodriguez <yarixarodriguez94@gmail.com>
2022-06-28 22:19:13 -0400

    commit tree bbeceb972f87ac8450e6ef4fdbfcd9f35d48ebe0

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
-const main = document.querySelector('main').remove();
-//main.remove();
-
+document.querySelector('main').remove();
 const newHeader = document.createElement("h1");
-document.body.append(newHeader);
-
-//newHeader.setAttribute("id", "victory");
-  newHeader.id = "victory";
-  newHeader.innerHTML = "Yari is the champion";
-  //document.querySelector(victory);
-  //victory.innerHTML = "YOUR-NAME is the champion"
-
-//   const textNode = document.createTextNode("YOUR-NAME is the champion");
-//   newHeader.appendChild(textNode);
-
-
-//   const text = document.createTextNode("<h1>YOUR-NAME is the champion!</h1>");
-//   newHeader.appendChild(text);
-//   document.getElementById('h1').id = 'victory'
+newHeader.id = "victory";
+newHeader.innerHTML = "YOUR-NAME is the champion";
